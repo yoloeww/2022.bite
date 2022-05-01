2a2c3e013b31395a294cd12998c4d71bbf6eda48
yolo <77111763+yoloeww@users.noreply.github.com>
2022-05-01 21:48:11 +0800

    Create 2022.5.1.大小.c

---
+++ 2022.5.1.大小.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main()
+{
+   int a,b,c,t;
+   scanf("%d%d%d",&a,&b,&c);
+   if(a>b)
+   {
+   t=b;
+   b=a;
+   a=t;
+   }
+   if(a>c)
+   {t=c;
+   c=a;
+   a=t;}
+   if(b>c)
+   {
+   t=c;
+   c=b;
+   b=t;
+   }
+   printf("%d %d %d",c,b,a);
+   return 0;
+}
