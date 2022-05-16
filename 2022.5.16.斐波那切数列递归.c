f05b8c4dee74d41dc30efe362f139d4ec5c7c9d4
yolo <77111763+yoloeww@users.noreply.github.com>
2022-05-16 21:11:20 +0800

    Create 2022.5.16.斐波那切数列递归.c

---
+++ 2022.5.16.斐波那切数列递归.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int f(int n)
+{
+  if(n <= 2)
+    return 1;
+  else
+    return f(n-1)+f(n-2);
+
+}
+int main()
+{
+ int n;
+ scanf("%d",&n);
+ int k=f(n);
+ printf("%d",n);
+ return 0;
+  
+}
