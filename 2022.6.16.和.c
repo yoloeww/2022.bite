11414a6f6806e82696d45af74ef8fb300826d192
yolo <77111763+yoloeww@users.noreply.github.com>
2022-06-16 22:44:34 +0800

    Create 2022.6.16.和.c

---
+++ 2022.6.16.和.c
@@ -0,0 +1,19 @@
+int main()
+{
+	int a = 0;
+	int n = 0;
+	int i = 0;
+	int sum = 0;
+	int tmp = 0;
+ 
+ 
+	scanf("%d%d", &a, &n);
+	for(i=0; i<n; i++)
+	{
+		tmp = tmp*10+a;
+		sum += tmp;
+	}
+	printf("%d\n", sum);
+ 
+	return 0;
+}
