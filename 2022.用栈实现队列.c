9a87dda644ce9fa20f51d6cf06bbceafc7873e0b
yolo <77111763+yoloeww@users.noreply.github.com>
2022-10-18 20:51:38 +0800

    Create 2022.用栈实现队列.c

---
+++ 2022.用栈实现队列.c
@@ -0,0 +1,28 @@
+asldtypedef struct {
+
+} MyQueue;
+
+
+MyQueue* myQueueCreate() {
+
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+
+}
+
+int myQueuePop(MyQueue* obj) {
+
+}
+
+int myQueuePeek(MyQueue* obj) {
+
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+
+}
+
+void myQueueFree(MyQueue* obj) {
+
+}
