5754cb35c66f0d735d83005140ef62ec96be17b2
yolo <77111763+yoloeww@users.noreply.github.com>
2022-10-27 09:51:36 +0800

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,15 +1,33 @@
-void InsertSort(int* a, int n)
+# include<stdio.h>
+# include<string.h>
+# include<assert.h>
+void BubbleSort(int* a, int n)
 {
-    for(int i = 0; i < n - 1;i++)
+    int i = 0 ,j  = 0;
+    int temp = 0,flag = 0;
+    for(i = 0 ;i< n - 1;i++)
     {
-        int end = i;
-        int tmp = a[end + 1];
-        while(end > = 0&&a[end] > tmp)
+        for(j = 0;j < n - i - 1;j++)
         {
-            a[end+1] =a [end];
-            end --;
+            if(a[j]<a[j+1])
+            {
+                temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+                flag = 1;
+            }
+            if(flag == 0 )
+            break;
         }
-        a[end+1]=tmp;
     }
-       
+}
+int main()
+{
+    int a[] = { 3, 4, 6, 1, 2, 8, 0, 5, 7 };
+    int n = sizeof(a) / sizeof(int);
+    BubbleSort(a,n);
+    for(int i = 0; i < n;i++)
+    {
+        printf("%d" ,a[i]);
+    }
 }
